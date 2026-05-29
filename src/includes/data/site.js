feb505b15bd8cb197632115d15ff211d9fb68129
Diederik <diederikdijkstra@hey.com>
2026-05-29 11:38:06 +0200

    Update stijling

--- src/includes/data/site.js
+++ src/includes/data/site.js
@@ -1,5 +1,5 @@
 export default {
-  name: "O&O Project van Elin, Esmée & Nynke",
+  name: "O&O Project van Elin,&nbsp;Esmée&nbsp;&&nbsp;Nynke",
   description: 'Sportclubs krijgen steeds vaker te maken met financiële problemen. Denk aan stijgende energiekosten, minder sponsoren, contributie die niet betaald wordt en hogere kosten voor materialen en onderhoud. Hierdoor kunnen clubs moeite krijgen om trainingen, wedstrijden en activiteiten te blijven organiseren. Vooral amateurverenigingen hebben hier last van. Daarom is het belangrijk dat sportclubs op tijd hulp krijgen en leren hoe ze slim met geld kunnen omgaan. Deze website helpt clubs met tips, oplossingen en informatie om financiële problemen te voorkomen en op te lossen.',
   lang: "nl",
   url: "https://sportclubs-financiele-problemen.netlify.app",
